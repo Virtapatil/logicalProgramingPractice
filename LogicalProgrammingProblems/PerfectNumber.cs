a6a966c33dc9829faab46a01a9e92ccdaeafbf87
Virtapatil2000 <virtapatil01@gmail.com>
2023-02-13 22:05:20 +0530

    [Virta] Adding Perfect Number

---
+++ LogicalProgrammingProblems/PerfectNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgrammingProblems
+{
+    internal class PerfectNumber
+    {
+        public static void PerfectNum()
+        {
+            int sum  = 0;
+            Console.WriteLine("Enter the number: ");
+            int num=Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            if (sum == num)
+            {
+                Console.WriteLine("The number is perfect number");
+            }
+            else
+            {
+                Console.WriteLine("The number is not perfect number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- LogicalProgrammingProblems/Program.cs
+++ LogicalProgrammingProblems/Program.cs
@@ -4,8 +4,24 @@
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello, World!");
-            FibonnciSeries.Fibo();
+            Console.WriteLine("Logical Programing Problem");
+            Console.WriteLine("Please Enter Below Number");
+            Console.WriteLine("1.FibonacciSeries \n2.PerfectNumber");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            
+            switch (choice)
+            {
+                case 1:
+                    FibonnciSeries.Fibo();
+                    break;
+                case 2:
+                    PerfectNumber.PerfectNum();
+                    break;
+                default:
+                    Console.WriteLine("Please Enter the option from Above");
+                    break;
+            }
+            Console.ReadLine();
         }
     }
 }
