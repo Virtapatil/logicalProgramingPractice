ec7bc3c415885658bea4c7c5e79623104ef54f3e
Virtapatil2000 <virtapatil01@gmail.com>
2023-02-13 20:21:51 +0530

    [Virta] Adding Fibonacci Series

---
+++ LogicalProgrammingProblems/FibonnciSeries.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgrammingProblems
+{
+    internal class FibonnciSeries
+    {
+        public static void Fibo()
+        {
+            int a = 0, b = 1, c;
+            Console.WriteLine("Enter the number:");
+            int num=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a + " " + b + " ");
+            for (int i = 2; i < +num; i++)
+            {
+                /*Console.Write("{0}", a);
+                c = a + b;
+                a = b;
+                b = c;*/
+                c = a + b;
+                Console.WriteLine(c + "");
+                a=b;
+                b=c;
+            }
+            Console.ReadLine();
+        }
+    }
+}
